f068c0c68d8fd64b1ef6c850e8eb0ec3aff35ac6
Razor <alexandergray37@gmail.com>
2020-03-06 15:40:21 -0800

    fixed test

--- recipes/tests.py
+++ recipes/tests.py
@@ -7,9 +7,9 @@ class PageTestCase(TestCase):
         """ Tests if True is equal to True. Should always pass. """
         self.assertEqual(True, True)
         
-    # def test_string_representation(self):
-    #     entry = Page(recipe_title="My entry title")
-    #     self.assertEqual(str(entry), Page.recipe_title)
+    def test_string_representation(self):
+        entry = Page(recipe_title="My entry title")
+        self.assertEqual(str(entry), entry.recipe_title)
         
     def test_verbose_name_plural(self):
         self.assertEqual(str(Page._meta.verbose_name_plural), "pages")
